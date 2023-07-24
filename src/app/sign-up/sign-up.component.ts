b5fa628f4e619c2a5706425d3f5cb544b54a3c76
Amr Mustafa <amrmostafa0366@gmail.com>
2023-07-24 18:13:08 +0200

    final-sign-up  fixed password

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -15,10 +15,10 @@ import { PhoneValidators } from '../validators/phone.validators';
 export class SignUpComponent {
   countryList = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
   citiesByCountry: { [country: string]: string[] } = {
-    "Afghanistan": ["Kabul", "Herat", "Mazar-i-Sharif", /* ... add cities for Afghanistan ... */],
-    "Albania": ["Tirana", "Durrës", "Vlorë", /* ... add cities for Albania ... */],
-    "Egypt": ["Cairo", "Alexandria", "Giza", "Sharm El Sheikh", /* ... add cities for Egypt ... */],
-    "Zimbabwe": ["Harare", "Bulawayo", "Chitungwiza", /* ... add cities for Zimbabwe ... */],
+    "Afghanistan": ["Kabul", "Herat", "Mazar-i-Sharif"],
+    "Albania": ["Tirana", "Durrës", "Vlorë"],
+    "Egypt": ["Cairo", "Alexandria", "Giza", "Sharm El Sheikh"],
+    "Zimbabwe": ["Harare", "Bulawayo", "Chitungwiza"],
   };
   user: any = {};
   userData = this.getUserFromLocalStorage();
@@ -119,7 +119,7 @@ export class SignUpComponent {
   ngOnInit(): void {
     if (this.userData) {
       this.form.patchValue({
-        name: this.userData.userName.firstName,
+        name: this.userData.userName.firstName +this.userData.userName.lastName,
         email: this.userData.userName.email,
         password: this.userData.userPassword.password,
         confirmPw: this.userData.userPassword.confirmPw,
@@ -131,7 +131,6 @@ export class SignUpComponent {
         militarySt: this.userData.userBirth.militarySt,
       });
     }
-    console.log(this.userData);
   }
 
   changeCountry() {
